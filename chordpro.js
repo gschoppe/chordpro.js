c10b633dbe0edfa9606659c4a003b332b5881f21
Greg Schoppe <greg@burlingtonbytes.com>
2016-06-21 16:24:59 -0400

    update to stand alone function, add basic command parsing

--- chordpro.js
+++ chordpro.js
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2014-16 Greg Schoppe <gschoppe@gmail.com>
  * Copyright (c) 2011 Jonathan Perkin <jonathan@perkin.org.uk>
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -14,107 +15,115 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/*
- * Project page:  http://github.com/jperkin/chordpro.js/
- * Personal page: http://www.perkin.org.uk/
- */
+/* Parse a ChordPro template */
+function parseChordPro(template) {
+	var chordregex= /\[([^\]]*)\]/;
+	var inword    = /[a-z]$/;
+	var buffer    = [];
+	var chords    = [];
+	var last_was_lyric = false;
+	if (!template) return;
 
-/*
- * Shamelessly based on mustache.js as I have no JS skills and likely Jan has
- * lots I can steal^Wre-use.  One day I might actually understand all this :)
- */
-var ChordPro = function() {
-  var Renderer = function() {};
-
-  Renderer.prototype = {
-    chordregex: /\[([^\]]*)\]/,
-    inword: /[a-z]$/,
-    buffer: [],
-
-    send: function(line) {
-      this.buffer.push(line);
-    },
-
-    /* Parse a ChordPro template */
-    parse: function(template) {
-      if (!template) {
-        return;
-      }
-      template.split("\n").forEach(function(line, linenum) {
-        /* Comment, ignore */
-        if (line.match(/^#/)) {
-          return;
-        }
-        /* Chord line */
-        if (line.match(this.chordregex)) {
-          var chords = "";
-          var lyrics = "";
-          var chordlen = 0;
-          line.split(this.chordregex).forEach(function(word, pos) {
-            var dash = 0;
-            /* Lyrics */
-            if ((pos % 2) == 0) {
-              lyrics = lyrics + word;
-              /*
-               * Whether or not to add a dash (within a word)
-               */
-              if (word.match(this.inword)) {
-                dash = 1;
-              }
-              /*
-               * Apply padding.  We never want two chords directly adjacent,
-               * so unconditionally add an extra space.
-               */
-              if (word && word.length < chordlen) {
-                chords = chords + " ";
-                lyrics = (dash == 1) ? lyrics + "- " : lyrics + "  ";
-                for (i = chordlen - word.length - dash; i != 0; i--) {
-                  lyrics = lyrics + " ";
-                }
-              }
-              else if (word && word.length == chordlen) {
-                chords = chords + " ";
-                lyrics = (dash == 1) ? lyrics + "-" : lyrics + " ";
-              }
-              else if (word && word.length > chordlen) {
-                for (i = word.length - chordlen; i != 0; i--) {
-                  chords = chords + " ";
-                }
-              }
-            }
-            /* Chords */
-            else {
-              chord = word.replace(/[[]]/, "");
-              chordlen = chord.length;
-              chords = chords + chord;
-            }
-          }, this);
-          this.send(chords + "\n" + lyrics);
-          return;
-        }
-        /* Commands, ignored for now */
-        if (line.match(/^{.*}/)) {
-          return;
-        }
-        /* Anything else */
-        this.send(line);
-      }, this);
-    },
-  };
-
-  return({
-    name: "chordpro.js",
-    version: "0.0.1",
-
-    to_txt: function(template) {
-      var renderer = new Renderer();
-      renderer.parse(template);
-      return renderer.buffer.join("\n");
-    }
-  });
-}();
-
-exports.name = ChordPro.name;
-exports.version = ChordPro.version;
-
-exports.to_txt = ChordPro.to_txt;
+	template.split("\n").forEach(function(line, linenum) {
+		/* Comment, ignore */
+		if (line.match(/^#/)) {
+			return;
+		}
+		/* Chord line */
+		if (line.match(chordregex)) {
+			if( !buffer.length ) {
+				buffer.push('<div class="lyric_block">');
+				last_was_lyric = true;
+			} else if( !last_was_lyric ) {
+				buffer.push('</div><div class="lyric_block">');
+				last_was_lyric = true;
+			}
+			var chords = "";
+			var lyrics = "";
+			var chordlen = 0;
+			line.split(chordregex).forEach(function(word, pos) {
+				var dash = 0;
+				/* Lyrics */
+				if ((pos % 2) == 0) {
+					lyrics = lyrics + word.replace(' ', "&nbsp;");
+				  /*
+				   * Whether or not to add a dash (within a word)
+				   */
+					if (word.match(this.inword)) {
+						dash = 1;
+					}
+				  /*
+				   * Apply padding.  We never want two chords directly adjacent,
+				   * so unconditionally add an extra space.
+				   */
+					if (word && word.length < chordlen) {
+						chords = chords + "&nbsp;";
+						lyrics = (dash == 1) ? lyrics + "-&nbsp;" : lyrics + "&nbsp&nbsp;";
+						for (i = chordlen - word.length - dash; i != 0; i--) {
+							lyrics = lyrics + "&nbsp;";
+						}
+					} else if (word && word.length == chordlen) {
+						chords = chords + "&nbsp;";
+						lyrics = (dash == 1) ? lyrics + "-" : lyrics + "&nbsp;";
+					} else if (word && word.length > chordlen) {
+						for (i = word.length - chordlen; i != 0; i--) {
+							chords = chords + "&nbsp;";
+						}
+					}
+				} else {
+					/* Chords */
+					chord = word.replace(/[[]]/, "");
+					chordlen = chord.length;
+					chords = chords + '<span class="chord" data-original-val="' + chord + '">' + chord + '</span>';
+				}
+			}, this);
+			buffer.push('<span class="line">' + chords + "<br/>\n" + lyrics + "</span><br/>");
+			return;
+		}
+		/* Commands, ignored for now */
+		if (line.match(/^{.*}/)) {
+			if( !buffer.length ) {
+				buffer.push('<div class="command_block">');
+				last_was_lyric = false;
+			} else if( last_was_lyric ) {
+				buffer.push('</div><div class="command_block">');
+				last_was_lyric = false;
+			}
+			//ADD COMMAND PARSING HERE
+			//reference: http://tenbyten.com/software/songsgen/help/HtmlHelp/files_reference.htm
+			// implement basic formatted text commands
+			var matches = line.match(/^{(title|t|subtitle|st|comment|c):\s*(.*)}/, "i");
+			if( matches.length >= 3 ) {
+				var command = matches[1];
+				var text = matches[2];
+				var wrap="";
+				//add more non-wrapping commands with this switch
+				switch( command ) {
+					case "title":
+					case "t":
+						command = "title";
+						wrap = "h1";
+						break;
+					case "subtitle":
+					case "st":
+						command = "subtitle";
+						wrap = "h4";
+						break;
+					case "comment":
+					case "c":
+						command = "comment";
+						wrap    = "em";
+						break;
+				}
+				if( wrap ) {
+					buffer.push('<' + wrap + ' class="' + command + '">' + text + '</' + wrap + '>' );
+				}
+			}
+			// work from here to add wrapping commands
+			return;
+		}
+		/* Anything else */
+		buffer.push(line + "<br/>");
+	}, this);
+	return buffer.join("\n");
+}
